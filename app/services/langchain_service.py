9a0e12c55c4e517f859057901e15f3f94913ffca
Joel Uzcategui <joeluzcategui@protonmail.com>
2025-07-29 19:36:34 +0100

    Fix agent

--- app/services/langchain_service.py
+++ app/services/langchain_service.py
@@ -74,7 +74,7 @@ class LangchainService:
     
     def __init__(self):
         self.llm = ChatOpenAI(
-            model_name="gpt-3.5-turbo",
+            model_name="gpt-4o",
             temperature=0.5,
             max_tokens=400,
             openai_api_key=settings.openai_api_key
